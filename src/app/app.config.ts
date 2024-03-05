df2a8553fe79065e7098312e5f2e2fa894fffeff
alva0128 <floresmendezalvaro@gmail.com>
2024-03-05 14:17:23 -0400

    primer commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,31 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+// import { provideHttpClient } from '@angular/common/http';
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzTRr-per6InoGGY4EMOEMzK8E2gOX53g",
+  authDomain: "testauth-293c3.firebaseapp.com",
+  projectId: "testauth-293c3",
+  storageBucket: "testauth-293c3.appspot.com",
+  messagingSenderId: "977934146477",
+  appId: "1:977934146477:web:5c0b60f158fe3f0e625f24",
+  measurementId: "G-X86JPMQNDX"
+};
+
+initializeApp(firebaseConfig);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes, withHashLocation()),
+  importProvidersFrom(
+    HttpClientModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
+  ) ]
 };
---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,100 @@
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+  standalone: true,
+  imports: [ FormsModule, CommonModule ],
+})
+export class SidebarComponent implements OnInit {
+  ngOnInit() {
+
+  }
+  activeSubMenuItemIndex: number | null = null; // Variable para mantener el índice del elemento de submenú activo
+
+  constructor() { }
+  sidebarActive: boolean = false;
+  svgActive: boolean[] = [];
+  activeSubMenu: number | null = null; // Variable para mantener el submenú activo
+  imagenUrl = '/assets/user.jpg';
+  svg = '/assets/flecha-pequena-izquierda.svg';
+  mainMenuItems: any[] = [
+    {
+      text: 'Dashboard',
+      iconClass: '/assets/add.svg',
+      subMenuItems: null
+    },
+    {
+      text: 'Audience',
+    iconClass: '/assets/add.svg',
+      subMenuItems: [
+        { text: 'Users' },
+        { text: 'Subscribers' }
+      ]
+    },
+    {
+      text: 'Posts',
+      iconClass: '/assets/add.svg',
+      subMenuItems: null
+    },
+    {
+      text: 'Schedules',
+      iconClass: '/assets/add.svg',
+      subMenuItems: null
+    },
+    {
+      text: 'Income',
+      iconClass: '/assets/add.svg',
+      subMenuItems: [
+        { text: 'Earnings' },
+        { text: 'Funds' },
+        { text: 'Declines' },
+        { text: 'Payouts' }
+      ]
+    },
+    {
+      text: 'Settings',
+      iconClass: '/assets/add.svg',
+      subMenuItems: [
+        { text: 'Settings' }
+      ]
+    }
+  ];
+
+  toggleSidebar(): void {
+    this.sidebarActive = !this.sidebarActive;
+  }
+
+  toggleSubMenu(index: number) {
+    if (this.activeSubMenu === index) {
+      this.activeSubMenu = null; // Si el submenú está abierto, ciérralo
+      this.svgActive[index] = false; // Desactiva la rotación
+    } else {
+      if (this.activeSubMenu !== null) {
+        this.svgActive[this.activeSubMenu] = false; // Desactiva la rotación del menú previamente abierto
+      }
+      this.activeSubMenu = index; // Abre el submenú correspondiente
+      this.svgActive[index] = true; // Activa la rotación
+    }
+  }
+  toggleSubMenuItem(subMenuItemIndex: number) {
+    // Activa el elemento del submenú correspondiente
+    this.activeSubMenuItemIndex = subMenuItemIndex;
+  }
+
+  toggleSvg(index: number) {
+    console.log('Toggle SVG llamado para el índice', index);
+    if (this.activeSubMenu !== index) {
+      this.svgActive[index] = !this.svgActive[index]; // Cambia el estado de la rotación solo si el submenú no está activo
+    }
+  }
+
+
+  closeSubMenu(event: Event) {
+    event.stopPropagation(); // Evita que el clic en el submenú se propague y active el cierre del submenú
+  }
+
+}
