947ab80374ad3421c8b57d257b731a9959e076a5
JulioPc <jcesar@loyaltyclubs.net>
2024-03-13 17:08:43 -0400

    avance

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,10 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 // import { provideHttpClient } from '@angular/common/http';
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from 'firebase/app';
+import { initializeApp as initializeApp_alias, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore'
 
 // const firebaseConfig = {
 //   apiKey: "AIzaSyCzTRr-per6InoGGY4EMOEMzK8E2gOX53g",
@@ -39,7 +42,7 @@ export const appConfig: ApplicationConfig = {
     HttpClientModule,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFirestoreModule
-  ) ]
+  ), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"test-cbntrade","appId":"1:164556829628:web:91ce830488533ef4afb1b9","storageBucket":"test-cbntrade.appspot.com","apiKey":"AIzaSyDbWH-Nxg2FuR9H1-yk7rjEVY6_XwG9juw","authDomain":"test-cbntrade.firebaseapp.com","messagingSenderId":"164556829628","measurementId":"G-H7B0ECN420"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())) ]
 
 };
 
